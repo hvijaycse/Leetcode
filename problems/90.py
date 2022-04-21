a846ca597552ad1b3b8836960ac6ccc9c53f8cfc
Harsh Vijay <hvijay.cse@gmail.com>
2022-04-21 23:42:43 +0530

    Updated solution for better explanation

--- problems/90.py
+++ problems/90.py
@@ -1,6 +1,8 @@
 from typing import List, Optional, Any, Dict
 
 
+
+
 class Solution:
 
     def subsets(self, nums: List[int], lo: int, hi: int) -> List[List[int]]:
@@ -9,24 +11,31 @@ class Solution:
         else:
             i = lo
             while i < hi and nums[i] == nums[lo]:
+                # Getting the index where nums[i] != num[lo]
                 i += 1
-            this_subset = [[nums[lo]]*time for time in range(1, i - lo + 1)]
+            
+            # Subset due to repeated numbers
+            # for eg:
+            #   "1" : ["1"]
+            #   "1","1": ["1"],["1", "1"]
 
-            lower_subset = self.subsets(nums, lo=i, hi=hi)
+            number_subet = [[nums[lo]]*time for time in range(1, i - lo + 1)]
 
-            mixed_subset = []
+            # Subset of remaining numbers
+            reamining_subset = self.subsets(nums, lo=i, hi=hi)
 
-            for item in this_subset:
-                for subset in lower_subset:
-                    mixed_subset.append(item + subset)
+            total_subset = []
 
-            return lower_subset + mixed_subset
+            # Creating list of all the subsets.
+            for item in number_subet:
+                for subset in reamining_subset:
+                    total_subset.append(item + subset)
+
+            return reamining_subset + total_subset
 
 
 
 
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-
         nums.sort()
-        
         return self.subsets(nums, 0, len(nums))
